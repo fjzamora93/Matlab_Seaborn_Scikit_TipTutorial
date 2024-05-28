ea74e284b345ba34fe33b6d6813b387a059dce5f
fjzamora93 <fjzamora93@gmail.com>
2024-05-28 18:27:31 +0200

    update

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from pandas import DataFrame
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 def mean_absolute_percentage_error(y_true, y_pred): 
@@ -27,15 +28,23 @@ def mae_porcentual (y_true, y_pred):
     return np.mean(porcentaje)
 
 
-def update_resultados(model:LinearRegression, X : DataFrame, y:DataFrame, y_pred: DataFrame, index_name: str, resultados = DataFrame({}) ) -> DataFrame:
+def update_resultados(model:LinearRegression, 
+                      X : DataFrame, y:DataFrame, 
+                      index_name: str, 
+                      resultados = DataFrame({}) 
+                    ) -> DataFrame:
     """
     Devuelve un DataFrame con la mae, la mae_porcentual, el mse, el rmse, y el coeficiente de determinación (R2).
+    Si no se le pasa ningún DataFrame de 'resultados' como parámetro, utiliza por defecto uno vacío.
     """
-    mae = mean_absolute_error(y, y_pred)
-    mae_porc = mae_porcentual (y, y_pred)
-    mse = mean_squared_error(y, y_pred)
-    rmse = mean_squared_error(y, y_pred, squared=False)
-    r2 = model.score(X,y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    y_pred = model.predict(X_test) 
+
+    mae = mean_absolute_error(y_test, y_pred)
+    mae_porc = mae_porcentual (y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = mean_squared_error(y_test, y_pred, squared=False)
+    r2 = model.score(X_train, y_train)
 
     nuevo_registro = pd.DataFrame({
         'MAE': [mae],
@@ -51,7 +60,7 @@ def update_resultados(model:LinearRegression, X : DataFrame, y:DataFrame, y_pred
         print(f"El modelo '{nuevo_registro.index[0]}' ya existe en los resultados.")
     return resultados
 
-def calcular_residuos(y : DataFrame, y_predic: DataFrame, col: str):
+def calcular_residuos(y : DataFrame, y_predic: DataFrame, col: str) -> None:
     """
     Dibuja en una gráfica los residuos (eje y) frente a los valores predichos. 
     Intentamos que la distribución sea lo más uniforme posible para evitar patrones o sesgos en los datos
@@ -62,3 +71,24 @@ def calcular_residuos(y : DataFrame, y_predic: DataFrame, col: str):
     sns.scatterplot(x=y_predic[:,0], y=residuos[f'{col}'])
     plt.ylabel('Residuos')
     plt.axhline(y=0, color='r', linestyle='--')
+
+
+def drop_outliers(df:DataFrame) -> DataFrame:
+    df_copy = df.copy()
+
+    for column in df_copy.columns:
+        if df_copy[column].dtype == 'object':
+            continue
+
+        # borrar outliers en numéricos
+        Q1 = df_copy[column].quantile(0.25)
+        Q3 = df_copy[column].quantile(0.75)
+        IQR = Q3 - Q1
+
+        lower_limit = Q1 - 1.5 * IQR
+        upper_limit = Q3 + 1.5 * IQR
+
+        filter = (df_copy[column] >= lower_limit) & (df_copy[column] <= upper_limit)
+        df_copy = df_copy[filter]
+
+    return df_copy
